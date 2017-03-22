21ab009997882ac4a3afc1b541916d88fe7aad59
DESKTOP-F28K8BC\pstan <pstan>
2017-03-22 11:48:01 -0400

    Added NLog, and the file location for the log "C:\logs\

---
+++ ExamAssessmentWebAPI/ExamAssessmentWebAPI/App_Start/NLogConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NLog;
+
+namespace ExamAssessmentWebAPI.App_Start
+{
+    public class NLogConfig
+    {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+    }
+}
--- ExamAssessmentWebAPI/ExamAssessmentWebAPI/Controllers/TestController.cs
+++ ExamAssessmentWebAPI/ExamAssessmentWebAPI/Controllers/TestController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using NLog;
 
 namespace LMS1701.EA.Controllers
 {
