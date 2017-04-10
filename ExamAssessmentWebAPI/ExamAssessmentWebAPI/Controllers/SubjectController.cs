c8e89fea74ce1404c2edd285825bb560b17b94b9
Dguy117 <Dguy117@DGUYSLAPRIG>
2017-04-10 10:57:25 -0400

    subject

--- ExamAssessmentWebAPI/ExamAssessmentWebAPI/Controllers/SubjectController.cs
+++ ExamAssessmentWebAPI/ExamAssessmentWebAPI/Controllers/SubjectController.cs
@@ -15,7 +15,7 @@ namespace LMS1701.EA.Controllers
         //Controller that will handle everything related to Subject Controller
     {
         WCF.Service1Client client = new WCF.Service1Client(); //readonly suggested
-                                                              // GET: api/Subject
+
 
         #region TODO
         // TODO:complete methods inside this region.
@@ -24,6 +24,12 @@ namespace LMS1701.EA.Controllers
 
 
         #endregion
+
+        /// <summary>
+        /// Gets all the subjects in the Database
+        /// </summary>
+        /// <endpoint>[HTTP GET]: api/Subject</endpoint>
+        /// <returns>List of WCF.Subjects </returns>
         public IEnumerable<WCF.Subject> Get()
         {
             List<WCF.Subject> test = new List<WCF.Subject>();
@@ -36,19 +42,36 @@ namespace LMS1701.EA.Controllers
         }
 
         // POST: api/Subject
+        /// <summary>
+        /// Makes a new subject
+        /// </summary>
+        /// <endpoint>[HTTP POST]: api/Subject</endpoint>
+        /// <param name="SubjectName">Name of the Subject</param>
         public void Post([FromBody]string SubjectName)
         {
             client.AddSubject(SubjectName);
         }
 
         // POST:
+        /// <summary>
+        /// Adds a New Category to a subject
+        /// </summary>
+        /// <endpoint>[HTTP POST]: api/Subject/AddExistingCategory?SubjectName={SubjectName}</endpoint>
+        /// <param name="SubjectName">Name of the Subject</param>
+        /// <param name="CategoryName">Name of the Category</param>
         [ActionName("AddExistingCategory")]
         public void AddNewCategory(string SubjectName, [FromBody]string CategoryName)
         {
             client.spAddExistingCategory(SubjectName, CategoryName);
         }
-           
-        // Delete:
+
+        // [HTTP PUT]
+        /// <summary>
+        /// Removes a category from a subject
+        /// </summary>
+        /// <endpoint>[HTTP PUT]: api/Subject/RemoveCategory/?SubjectName={SubjectName}</endpoint>
+        /// <param name="SubjectName">Name of the Subject</param>
+        /// <param name="CategoryName">Name of the Category</param>
         [ActionName("RemoveCategory")]
         public void RemoveCategory(string SubjectName, [FromBody]string CategoryName)
         {
@@ -56,6 +79,11 @@ namespace LMS1701.EA.Controllers
         }
 
         // DELETE: api/Subject/5
+        /// <summary>
+        /// deletes a subject
+        /// </summary>
+        /// <endpoint>[HTTP DELETE]: api/Subject?SubjectName={SubjectName}</endpoint>
+        /// <param name="SubjectName">Name of the Subject</param>
         public void Delete(string SubjectName)
         {
             client.DeleteSubject(SubjectName);
